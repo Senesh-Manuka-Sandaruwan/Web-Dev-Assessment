f9297b2b847fa4411ff477016730a15aacc5e217
SENESH MANUKA SANDARUWAN <152684965+Senesh-Manuka-Sandaruwan@users.noreply.github.com>
2024-04-06 23:10:38 +0530

    check 11

--- js/store/home.js
+++ js/store/home.js
@@ -3,15 +3,15 @@ document.addEventListener('alpine:init', () => {
         is_editor_visible: false,
         block_six: [
             {
-                image1: '../images/SS1.jpg',
+                image1: 'https://github.com/Senesh-Manuka-Sandaruwan/Web-Dev-Assessment/blob/main/images/SS1.jpg?raw=true',
                 title1: 'Welcome to Sri Lankan Wildlife',
             },
             {
-                image2: 'https://Web-Dev-Assessment/Web-Dev-Assessment/images/SS2.jpg',
+                image2: 'images/SS2.jpg',
                 title2: 'Welcome to Sri Lankan Wildlife',
             },
             {
-                image3: 'https://github.com/Senesh-Manuka-Sandaruwan/Web-Dev-Assessment/blob/main/images/SS3.jpg',
+                image3: 'images/SS3.jpg',
                 title3: 'Welcome to Sri Lankan Wildlife',
             },
         ],
